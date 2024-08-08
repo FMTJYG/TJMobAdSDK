d5de4adbfd811911ab1510a3883b9e13428d37bc
YG <yangguang@qingting.fm>
2024-08-08 14:22:45 +0800

    add framework

---
+++ TJMobAdSDK.framework/Headers/TJMobAdSDK.h
@@ -0,0 +1,18 @@
+//
+//  TJMobAdSDK.h
+//  TJMobAdSDK
+//
+//  Created by GG on 2024/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TJMobAdSDK.
+FOUNDATION_EXPORT double TJMobAdSDKVersionNumber;
+
+//! Project version string for TJMobAdSDK.
+FOUNDATION_EXPORT const unsigned char TJMobAdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJMobAdSDK/PublicHeader.h>
+
+#import <TJMobAdSDK/TJMobAdSplash.h>
---
+++ TJMobAdSDK.framework/Headers/TJMobAdSplash.h
@@ -0,0 +1,16 @@
+//
+//  TJMobAdSplash.h
+//  TJMobAdSDK
+//
+//  Created by GG on 2024/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TJMobAdSplash : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
